273453057236e4dccf1b2dde698137ee31d27554
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-11-08 22:32:26 +0200

    Added DTO layer for Category

--- src/main/java/com/foalex/bookstore/dto/book/BookDto.java
+++ src/main/java/com/foalex/bookstore/dto/book/BookDto.java
@@ -1,17 +1,16 @@
 package com.foalex.bookstore.dto.book;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
-import java.util.List;
-import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
-@Data
-public class BookDto {
-    private Long id;
-    private String title;
-    private String author;
-    private String isbn;
-    private BigDecimal price;
-    private String description;
-    private String coverImage;
-    private List<Long> categoryIds;
+public record BookDto(
+        @NotBlank @Length(max = 128) String title,
+        @NotBlank @Length(max = 128) String author,
+        @NotBlank @Length(max = 128) String isbn,
+        @NotNull @Positive BigDecimal price,
+        @Length(max = 1024) String description,
+        @Length(max = 128) String coverImage) {
 }
